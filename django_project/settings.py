47253420fc034930714039e475bfcffd92d961ef
kartik modi <kartikm1198@gmail.com>
2021-09-05 17:59:18 +0530

    fix

--- django_project/settings.py
+++ django_project/settings.py
@@ -152,5 +152,5 @@ LOGIN_URL = 'login'
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 django_heroku.settings(locals())
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, 'staticfiles'),
 )
